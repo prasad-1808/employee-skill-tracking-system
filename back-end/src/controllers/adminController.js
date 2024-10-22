bd44dc8c11eb6b5ac91166247fbf7781e4b1a663
Prasad.D <prasadvenki60@gmail.com>
2024-10-22 10:24:16 +0530

    commit 1

--- back-end/src/controllers/adminController.js
+++ back-end/src/controllers/adminController.js
@@ -3,9 +3,41 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // Register Admin
+// const adminregister = async (req, res) => {
+//   console.log(req.body);
+//   let { AdminID, AdminName, AdminEmail, AdminPassword } = req.body;
+//   const hashedPassword = await bcrypt.hash(AdminPassword, 10);
+
+//   try {
+//     const admin = await prisma.admin.create({
+//       data: {
+//         AdminID,
+//         AdminName,
+//         AdminEmail,
+//         AdminPassword: hashedPassword,
+//       },
+//     });
+
+//     res.status(201).json(admin);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// };
 const adminregister = async (req, res) => {
   console.log(req.body);
   let { AdminID, AdminName, AdminEmail, AdminPassword } = req.body;
+
+  // Regular expression to check password: 6-12 characters, at least 1 uppercase letter and 1 number
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,12}$/;
+
+  // Validate password strength
+  if (!passwordRegex.test(AdminPassword)) {
+    return res.status(400).json({
+      error:
+        "Password must be 6-12 characters long, contain at least 1 uppercase letter, and at least 1 number.",
+    });
+  }
+
   const hashedPassword = await bcrypt.hash(AdminPassword, 10);
 
   try {
